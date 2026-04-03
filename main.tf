3d23cc01e6132c8bdf2e97e9f31de6c4d007c785
TAS <cctuaungshin@gmail.com>
2026-04-03 07:48:52 +0630

    rename subnet name

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
 }
 
 # Create a public subnet
-resource "aws_subnet" "my_subnet" {
+resource "aws_subnet" "my_subneta" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-southeast-1a"
@@ -20,7 +20,7 @@ resource "aws_subnet" "my_subnet" {
 }
 
 # Create a public subnet
-resource "aws_subnet" "my_subnet" {
+resource "aws_subnet" "my_subnetb" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "ap-southeast-1b"
@@ -31,7 +31,7 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 # Create a public subnet
-resource "aws_subnet" "my_subnet" {
+resource "aws_subnet" "my_subnetc" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = "ap-southeast-1c"
