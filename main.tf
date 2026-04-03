2d7e97765a94815ac9c4b84bd659dde287ab4568
TAS <cctuaungshin@gmail.com>
2026-04-03 07:45:42 +0630

    add subnet

--- main.tf
+++ main.tf
@@ -6,3 +6,38 @@ resource "aws_vpc" "main" {
     Name = "main-vpc"
   }
 }
+
+# Create a public subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
+
+# Create a public subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-southeast-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
+# Create a public subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "ap-southeast-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
